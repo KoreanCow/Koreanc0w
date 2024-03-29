8486be14bdf90bb8b844b337322a8e3e3790c207
KoreanCow <vue-cli@kakao.com>
2024-03-29 18:47:37 +0900

    Feat: add  MainPage Layout | my profile img and change tailwindcss to scss

--- client/src/app/(main)/page.tsx
+++ client/src/app/(main)/page.tsx
@@ -6,29 +6,51 @@ export default function Home() {
 
   return (
     <>
-      <div className=' h-40 bg-white' />
-      <div className='main-page--container'>
-        <p className='main-page--container-text'>KOREAN COW</p>
+      {/* <div className='h-40' /> */}
+      <div className='main-page--top-container'>
+        <p className='main-page--top-container-text'>KOREAN COW</p>
         <Image
           src="/imgs/IMG_2434.jpeg"
           alt="mainimg"
           draggable={false}
           width={2016}
           height={1512}
-          className="main-page--container-main-img"
+          className="main-page--top-container-main-img"
           priority
         />
       </div>
-      <div className='w-full border-b-5 h-5 border-b border-title-color'>
-        <div className=' w-1/2 h-5 bg-light-title border-b  border-title-color ' />
+      <div className='main-page--top-container-classfication'>
+        <div className='main-page--top-container-classfication-progress' />
       </div>
-      <div className='w-full h-400 bg-black'>
+      <div className='main-page--profile-container'>
+        {/* <div className='w-91 flex justify-between items-center'> */}
+        <div className='main-page--profile-media-query-box'>
+          <div className='main-page--profile-summary'>
+            <p>프로그래머 프로필</p>
+            <span>부가설명 1</span>
+            <span>부가설명 2</span>
+          </div>
+          <Image
+            src='/imgs/profile.png'
+            alt='profile img'
+            draggable={false}
+            width={240}
+            height={240}
+            className='main-page--profile-img'
+            priority
+          />
+          {/* <div className='main-page--profile-img'>
+
+          </div> */}
+        </div>
+        <div>
+
+        </div>
+      </div>
+      <div className='main-page--post'>
 
       </div>
-      <div className='w-full h-1800 bg-lime-300 border-b-2 border-light-title'>
-
-      </div>
-      <div className='w-full h-16 mt-9 bg-light-title'>
+      <div className='main-page--bottom-container'>
 
       </div>
     </>
