976bd869bfe6c89597d60158964346183aa4cc63
KoreanCow <vue-cli@kakao.com>
2024-03-29 18:45:55 +0900

    Feat: globals css variable and height 160px div tag

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navigation />
+        <div className='h-40' />
         {children}
       </body>
     </html>
