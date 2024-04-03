bf10cd00adaa347801ad734edb26d5a62741a8e0
KoreanCow <vue-cli@kakao.com>
2024-04-03 17:16:32 +0900

    Feat: solve the error - nested layout | Add blog page layout

--- client/src/app/(blog)/blog/layout.tsx
+++ client/src/app/(blog)/blog/layout.tsx
@@ -1,31 +1,11 @@
-
-import { Inter } from "next/font/google";
-import Navigation from '@/components/navigation';
-
-const inter = Inter({ subsets: ["latin"] });
-
-// export const metadata: Metadata = {
-//   title: {
-//     default: "KoreanCow | COWLOG",
-//     template: "KoreanCow | COWLOG | %s"
-//   },
-//   description: "COWLOG with Next.js",
-//   icons: {
-//     icon: '/favicon-32x32.png',
-//   }
-// };
-
-export default function RootLayout({
+export default function BlogLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Navigation />
-        {children}
-      </body>
-    </html>
+    <div>
+      {children}
+    </div>
   );
 }
--- client/src/app/(blog)/blog/page.tsx
+++ client/src/app/(blog)/blog/page.tsx
@@ -1,12 +1,30 @@
+import Image from 'next/image';
+
 export const metadata = {
   title: 'Blog',
 };
 
 export default function Blog() {
   return (
-    <>
-      <p>dsasds</p>
-    </>
+    <div>
+      <div>
+        <p></p>
+        {/* <Image /> */}
+      </div>
+
+      <div>
+        <div>
+          side bar
+        </div>
+        <div>
+          recent post
+          <div>
+
+          </div>
+        </div>
+      </div>
+
+    </div>
   );
 
 }
